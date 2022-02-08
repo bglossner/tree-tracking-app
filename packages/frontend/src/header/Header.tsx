5afa7af6572a6afd2753445775b6a20148119f80
Runchimchar <jpwolf101@gmail.com>
2022-02-07 21:45:29 -0800

    Added Header and updated global vars

---
+++ packages/frontend/src/header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+export const Header = () => {
+  return (
+    <header className="App-header">
+        <p>
+          <a href='#TreeCounter'>Tree Counter</a> | <a href='#About'>About</a> | <a href='#Map'>Map</a>
+        </p>
+    </header>
+  );
+};
