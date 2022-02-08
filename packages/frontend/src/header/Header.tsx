7eafbdae23de06096c5978cec1d73425e6a379c4
Runchimchar <jpwolf101@gmail.com>
2022-02-07 23:26:23 -0800

    Fixed formatting issues

--- packages/frontend/src/header/Header.tsx
+++ packages/frontend/src/header/Header.tsx
@@ -3,9 +3,9 @@ import './Header.scss';
 export const Header = () => {
   return (
     <header className="App-header">
-        <p>
+        <nav>
           <a href='#TreeCounter'>Tree Counter</a> | <a href='#About'>About</a> | <a href='#Map'>Map</a>
-        </p>
+        </nav>
     </header>
   );
 };
