99f72ecc153be903405b2f1819d60f3ca715d498
bglossner <benglossner@gmail.com>
2022-02-15 10:48:42 -0800

    feat: Added Survey123 embedded form

---
+++ packages/frontend/src/new-tree/survey123-interface.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+export const generateForm = (container, onFormLoad, onFormSubmit, onFormFail) => {
+  const webform = new Survey123WebForm({
+    clientId: "YUnfvUenRS5bzPOW",
+    container: container,
+    itemId: "ac2cf4bda3fb49e7bf4860b23850ec86",
+    onFormLoaded: (data) => {
+      onFormLoad(webform, data);
+    },
+    onFormSubmitted: (data) => {
+      onFormSubmit(webform, data);
+    },
+    hideElements: ['navbar', 'footer'],
+  });
+
+  webform.setOnFormFailed(data => {
+    onFormFail(webform, data);
+  });
+};
--- packages/frontend/src/new-tree/tree-tracking-form.tsx
+++ packages/frontend/src/new-tree/tree-tracking-form.tsx
@@ -1,140 +1,82 @@
+import { useEffect, useState } from 'react';
+import { generateForm } from './survey123-interface';
 import './tree-tracking-form.scss';
-import { Controller, useForm } from "react-hook-form";
-import * as exifr from "exifr";
-import { SyntheticEvent, useEffect, useState } from "react";
-import CreatableSelect from 'react-select/creatable';
-import { FormField } from '../util/forms/FormField';
 
-type TreeTrackingFormProps = {
-};
+const SURVEY_DIV_CONTAINER = "survey-container";
 
-const isImage = (file: File) => file['type'].includes('image');
+type FormDataEventFn = (webform: any, data: any) => void;
 
-type GPSResult = { latitude: number, longitude: number } | undefined;
-
-const getGpsCoords = async (file: File) => new Promise<GPSResult>((resolve, reject) => {
-  const reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onload = async function () {
-    // console.log(reader.result);
-    const res = reader.result as string;
-    const gpsResults = await exifr.gps(res);
-    resolve(gpsResults);
+const onFormLoaded = (formLoadedCallback: FormDataEventFn) => {
+  return (webform: any, data: any) => {
+    // taken from https://developers.arcgis.com/survey123/guide/embed-a-survey-using-javascript/
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        webform.setGeopoint({
+          x: position.coords.longitude,
+          y: position.coords.latitude
+        });
+      }, () => { console.error('Unable to retrieve your location'); });
+    } else {console.error('Geolocation is not supported by your browser'); }
+    
+    formLoadedCallback(webform, data);
   };
-  reader.onerror = function (error) {
-    reject(error);
+};
+
+const onFormSubmitted = (formSubmittedCallback: FormDataEventFn, formSubmittedFailureCallback: FormDataEventFn) => {
+  return (webform: any, data: any) => {
+    console.log(data);
+    if (data.error?.message) {
+      formSubmittedFailureCallback(webform, data);
+    } else {
+      formSubmittedCallback(webform, data);
+    }
+  }
+};
+
+const onFormFailed = (formFailedCallback: FormDataEventFn) => {
+  return (webform: any, data: any) => {
+    console.log(data);
+    formFailedCallback(webform, data);
+  }
+};
+
+export interface ITreeTrackingFormProps {
+  onNewTreeSubmission?: (data: any) => void;
+};
+
+export const TreeTrackingForm = ({ onNewTreeSubmission }: ITreeTrackingFormProps) => {
+  const [formLoaded, setFormLoaded] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [formSubmitError, setFormSubmitError] = useState(false);
+  const [formFailed, setFormFailed] = useState(false);
+
+  const onFormSubmission = (_webform: any, data: any) => {
+    setFormSubmitError(false);
+    setFormSubmitted(true);
+
+    if (onNewTreeSubmission) {
+      onNewTreeSubmission(data);
+    }
   };
-});
-
-const treeSpecies: string[] = [
-  "redwood",
-  "pine",
-  "spruce",
-  "sequoia",
-  "palm",
-  "two words",
-];
-
-interface SelectOption {
-  value: string;
-  label: string;
-}
-
-const capitalize = (s: string) => {
-  return s.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
-};
-
-const speciesListToSelectOptions = (species: string[]): SelectOption[] => {
-  return species.map(s => ({ value: s.toLowerCase(), label: capitalize(s) }))
-};
-
-export const TreeTrackingForm: React.FC<TreeTrackingFormProps> = (props) => {
-  const { register, control, handleSubmit, getValues, setError, formState: { errors, isSubmitting } } = useForm();
-  const [species, setSpecies] = useState<string[]>([]);
-  const [speciesAsOptions, setSpeciesAsOptions] = useState<SelectOption[]>([]);
 
   useEffect(() => {
-    setSpecies(treeSpecies);
-    setSpeciesAsOptions(speciesListToSelectOptions(treeSpecies));
+    // Don't rerender form if it's already on page
+    if (!document.getElementById(SURVEY_DIV_CONTAINER)?.querySelector('iframe')) {
+      generateForm(
+        SURVEY_DIV_CONTAINER,
+        onFormLoaded(() => setFormLoaded(true)),
+        onFormSubmitted(onFormSubmission, () => setFormSubmitError(true)),
+        onFormFailed(() => setFormFailed(true)));
+    }
   }, []);
 
-  const [photoObject, setPhotoObject] = useState<string | undefined>(undefined);
-
-  const onSubmit = (values: any) => {
-    console.log(values);
-  };
-
-  const onTreeFileUpload = async (e: SyntheticEvent): Promise<void> => {
-    const target = e.target as HTMLInputElement;
-
-    if (!target.files) {
-      return;
-    }
-
-    const treePhoto = target.files[0];
-
-    if (!isImage(treePhoto)) {
-      // handle bad image
-      setError("treeImage", {
-        type: "invalidImage",
-        message: "That is not a valid image!"
-      });
-      return;
-    }
-
-    setPhotoObject(URL.createObjectURL(treePhoto));
-
-    const gpsCoords = await getGpsCoords(treePhoto).catch(e => Error(e));
-    if (gpsCoords instanceof Error) {
-      // handle error here
-      return;
-    }
-
-    if (!gpsCoords) {
-      // handle bad image format
-      return;
-    }
-
-    const { latitude, longitude } = gpsCoords;
-    console.log(latitude, longitude);
-  };
-
-  console.log(errors);
+  // some change
 
   return (
-    <form id="new-tree" onSubmit={handleSubmit(onSubmit)}>
-      <FormField label="Planter (name of group/person)" errors={errors.planter}>
-        <input {...register("planter", { required: true })} type="text" />
-      </FormField>
-      <FormField errors={errors.treeSpeciesSelector} label="Tree Species">
-        <Controller
-          control={control}
-          name="treeSpeciesSelector"
-          rules={{ required: true }}
-          render={({
-            field: { onChange },
-          }) => (
-            <CreatableSelect
-              isClearable
-              onChange={onChange}
-              defaultValue={speciesAsOptions?.[0]}
-              options={speciesAsOptions}
-            />
-          )}
-        />
-      </FormField>
-      <br />
-      {/* perhaps give a drap/drop too */}
-      <FormField label="Photo(s) of Tree" errors={errors.treeImage}>
-        <input {...register("treeImage", { onChange: onTreeFileUpload })} type="file" />
-      </FormField>
-      {
-        !!getValues("treeImage") && !errors.treeImage && photoObject ? (
-          <img className="tree-photo" src={photoObject} alt="Uploaded tree." />
-        ) : null
-      }
-
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      {!formLoaded && !formFailed && <p style={{ fontSize: "3em" }}>The form is still loading. Please wait!</p>}
+      {formFailed && <p>The form failed to load due to some third party cookie issues. Please enable cookies!</p>}
+      <div id={SURVEY_DIV_CONTAINER} />
+    </>
   );
-}
+};
