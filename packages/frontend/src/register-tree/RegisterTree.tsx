58c9cdaefe0ed49715d67b45265bf9af935ab1ea
bglossner <benglossner@gmail.com>
2022-02-22 14:25:16 -0800

    feat: Add tree counter to home page and buttons for registering tree

--- packages/frontend/src/register-tree/RegisterTree.tsx
+++ packages/frontend/src/register-tree/RegisterTree.tsx
@@ -9,13 +9,14 @@ export const RegisterTree = () => {
         <Row className='counter-welcome'><h1>Welcome to the Urban Forest of San Luis Obispo</h1></Row>
         <Row>
           <Col className='counter-holder'>
-            <CountUp className='counter' end={2124} duration={2} separator=',' useEasing={true}/>
+            <CountUp className='counter' delay={0.5} end={2124} duration={2} separator=',' useEasing={true}/>
             <p>Trees Planted</p>
           </Col>
         </Row>
       </Container>
-      <div className='content'>
-
+      <div className='tree-action-btns-container'>
+        <a href='/new-tree' className='tree-action'>Register a Tree</a>
+        <a href='/adopt-tree' className='tree-action'>Adopt a Tree</a>
       </div>
     </div>
   );
