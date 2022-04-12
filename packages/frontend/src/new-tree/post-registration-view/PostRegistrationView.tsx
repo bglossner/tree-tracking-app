6161ac23a545c52b6ef0783b53b649c1f3b311fe
bglossner <benglossner@gmail.com>
2022-04-11 21:40:00 -0700

    feat(frontend): More verification links and better post registration page

--- packages/frontend/src/new-tree/post-registration-view/PostRegistrationView.tsx
+++ packages/frontend/src/new-tree/post-registration-view/PostRegistrationView.tsx
@@ -9,6 +9,7 @@ interface IProps {
 }
 
 export const PostRegistrationView = ({ treeNumber, submitAnotherTree }: IProps) => {
+  console.log('loading post registration');
   return (
     <main id="post-reg-view">
       <h2>You just planted tree<br /><span className="tree-number"># {treeNumber}</span></h2>
@@ -23,9 +24,12 @@ export const PostRegistrationView = ({ treeNumber, submitAnotherTree }: IProps) 
       <p>
         Thank you for making our community more beautiful! Your tree has been recorded and is helping SLO become more carbon neutral!
       </p>
+      <p style={{ fontSize: '1.25em' }}>
+        Remember to give your tree 5 gallons of water per week.
+      </p>
       <div className='post-reg-actions'>
         <HashLink
-          to="/#Map"
+          to={`/?treeNumber=${treeNumber}#Map`}
           className="post-reg-button view-map-btn"
         >
           View on Map
