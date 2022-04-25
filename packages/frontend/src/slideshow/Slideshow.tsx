a1178778d266919a1b11961f045f20d6ff9b17ca
bglossner <benglossner@gmail.com>
2022-04-24 23:57:29 -0700

    slideshow update

--- packages/frontend/src/slideshow/Slideshow.tsx
+++ packages/frontend/src/slideshow/Slideshow.tsx
@@ -1,41 +1,56 @@
 import { useEffect } from 'react';
+import { BasicTextSlideView } from './basic-text-slideview/BasicTextSlideView';
+import { PartnersSlide } from './logo-slideview/PartnersSlide';
 import './Slideshow.scss';
+
+/* Define abstract slide */
+interface ISlide {
+  slideView: React.ReactNode;
+  imageSrc: string,
+  labelText: string
+}
+
+/* Create slide list */
+const slideList: ISlide[] = [
+  {
+    slideView: (
+      <BasicTextSlideView
+        text="As part of San Luis Obispo's initiative to be Carbon Neutral by 2023, we are collectively planting 10,000 trees by 2035."
+      />),
+    imageSrc: "/images/slide1.png",
+    labelText: "MISSION"
+  },
+  {
+    slideView: (
+      <BasicTextSlideView
+        text="Want to plant a tree but don't know how? Don't worry! We have all the resources for you to have a successful planting experience."
+      />
+    ),
+    imageSrc: "/images/slide2.png",
+    labelText: "HOW TO PLANT"
+  },
+  {
+    slideView: (
+      <BasicTextSlideView
+        text="Not comfortable with planting your own tree? Sign up to adopt a tree! By adopting, you will take care of the tree you sign-up for by watering and more. We appreciate your help!"
+      />
+    ),
+    imageSrc: "/images/slide3.png",
+    labelText: "ADOPTING A TREE"
+  },
+  {
+    slideView: (
+      <PartnersSlide />
+    ),
+    imageSrc: "/images/slide4raw.png",
+    labelText: "OUR PARTNERS"
+  }
+];
 
 /* Slideshow modified from https://www.w3schools.com/howto/howto_js_slideshow.asp */
 
 export const Slideshow = () => {
   var slideIndex: number = 0;
-
-  /* Define abstract slide */
-  interface ISlide {
-    text: string,
-    imageSrc: string,
-    labelText: string
-  }
-
-  /* Create slide list */
-  const slideList: ISlide[] = [
-    {
-      text: "As part of the mission for San Luis Obispo to be Carbon Neutral by 2035, we are on a journey to plant 10,000 trees in the city by 2035.",
-      imageSrc: "/images/slide1.png",
-      labelText: "MISSION"
-    },
-    {
-      text: "Want to plant a tree but don’t know how? Don’t worry! We have all the resources for you to have a successful planting experience.",
-      imageSrc: "/images/slide2.png",
-      labelText: "HOW TO PLANT"
-    },
-    {
-      text: "Not comfortable with planting your own tree? Sign up to adopt a tree! By adopting, you will take care of the tree you sign-up for by watering and more. We appreciate your help!",
-      imageSrc: "/images/slide3.png",
-      labelText: "ADOPTING A TREE"
-    },
-    {
-      text: "Want to help in other ways? Check out resources from our partners.",
-      imageSrc: "/images/slide4logos.png",
-      labelText: "OUR PARTNERS"
-    }
-  ];
 
   // Called when an arrow is clicked
   const arrowClickHandler = (event: React.MouseEvent<HTMLElement>) => {
@@ -45,15 +60,12 @@ export const Slideshow = () => {
     const arrow = event.currentTarget;
 
     // Change slide
-    if (arrow.className == "next") {
+    if (arrow.className === "next") {
       slideIndex = (slideIndex + 1) % slideList.length;
     } else {
       slideIndex = (slideIndex + slideList.length - 1) % slideList.length;
     }
     changeSlide(slideIndex);
-
-    // Debug
-    console.log("Clicked arrow \"",arrow.className,"\", index is ",slideIndex);
   };
 
   // Called when a dot is clicked
@@ -64,8 +76,6 @@ export const Slideshow = () => {
     // Change slide
     slideIndex = index;
     changeSlide(slideIndex);
-
-    console.log("Clicked dot ",index,", index is now ",slideIndex);
   };
 
   // Does the work of changing a slide
@@ -95,8 +105,6 @@ export const Slideshow = () => {
     slide.style.display = "block";  
     dots[index].className += " active";
     
-    // Debug
-    console.log("Slide changed to ",index);
   }
 
   /* Preload images */
@@ -113,12 +121,13 @@ export const Slideshow = () => {
 
       {
       /* Create slides from list */
-      slideList.map(({text, imageSrc, labelText}, index, arr) => (
+      slideList.map(({slideView, imageSrc, labelText}, index, arr) => (
         <div className="slide">
           <div className="numbertext">{index+1} / {arr.length}</div>
           <div className="labelText">{labelText}</div>
-          <img src={imageSrc}/>
-          <div className="text">{text}</div>
+          <img className='bg-image' src={imageSrc} />
+          
+          {slideView}
         </div>
       ))
       }
