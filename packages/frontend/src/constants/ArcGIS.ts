9c9b79ccc1ee52516817d4e3df044b8bfa1747b2
efreeddo <efreeddo@calpoly.edu>
2022-05-16 09:45:36 -0700

    commented long map url

--- packages/frontend/src/constants/ArcGIS.ts
+++ packages/frontend/src/constants/ArcGIS.ts
@@ -3,3 +3,4 @@ export const SURVEY_ITEM_ID = '205ff9bd93a549d7b9b525f7522bcf28';
 export const PUBLIC_VIEW_URL = 'https://services1.arcgis.com/0j6vZbECadDEXdAS/arcgis/rest/services/Public_Field_View/FeatureServer/0';
 export const VERIFICATION_VIEW_URL = 'https://services1.arcgis.com/0j6vZbECadDEXdAS/arcgis/rest/services/survey123_205ff9bd93a549d7b9b525f7522bcf28/FeatureServer/0';
 export const MAP_URL = 'https://arcg.is/1mf5j00'; //'//calpoly.maps.arcgis.com/apps/Embed/index.html?webmap=4c073f94e5804f2b999b4f1ac6b928b9&zoom=true&previewImage=false&disable_scroll=true&theme=light&center=-120.66,35.28&level=12';
+//https://calpoly.maps.arcgis.com/apps/View/index.html?appid=e8f318a6b24242ef816afc2740fdb19a <- non shortened web app link
