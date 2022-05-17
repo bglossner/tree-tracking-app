9fe2830f933eb4f2a0418bb9a4e5d4dbe1ceaa71
bglossner <benglossner@gmail.com>
2022-05-16 20:50:18 -0700

    feat: Make map auto-focus based on tree number from URL

--- packages/frontend/src/map-contents/Map/MapSLO.tsx
+++ packages/frontend/src/map-contents/Map/MapSLO.tsx
@@ -1,10 +1,23 @@
-import { MAP_URL } from '../../constants/ArcGIS';
-import './MapSLO.scss'
+import { LENGTHENED_MAP_URL, MAP_URL } from '../../constants/ArcGIS';
+import { useQuery } from '../../util/hooks/useQuery';
+import './MapSLO.scss';
+
+export const TREE_NUMBER_QUERY_PARAM = 'tree_number';
+
+function getMapUrl(query: string | null) {
+  if (query) {
+    return `${LENGTHENED_MAP_URL}&${TREE_NUMBER_QUERY_PARAM}=${query}`;
+  } else {
+    return `${MAP_URL}`;
+  }
+}
 
 export const MapSLO = () => {
+  const query = useQuery();
+
   return (
     <div className="embed-container">
-      <iframe scrolling="no" title="10000trees2" src={MAP_URL}></iframe>
+      <iframe scrolling="no" title="10000 trees searchable map" src={getMapUrl(query.get(TREE_NUMBER_QUERY_PARAM))} />
     </div>
   );
 };
