fd601e40a556052f4d62d614c980922602275926
bglossner <benglossner@gmail.com>
2022-05-23 09:58:24 -0700

    make recently planted clickable

--- packages/frontend/src/map-contents/map-content.tsx
+++ packages/frontend/src/map-contents/map-content.tsx
@@ -1,6 +1,3 @@
-import { getAttachments, queryFeatures } from '@esri/arcgis-rest-feature-layer';
-import { useEffect } from 'react';
-import { MAP_URL, PUBLIC_VIEW_URL } from '../constants/ArcGIS';
 import './map-content.scss';
 import { MapSLO } from './Map/MapSLO';
 import { PlantedDashboard } from './planted-dashboard/PlantedDash';
@@ -10,11 +7,6 @@ interface IProps {
 }
 
 export const MapContent = ({ currentPlanted }: IProps) => {
-
-  useEffect(() => {
-    // fetch(`${PUBLIC_VIEW_URL}/queryAttachments?f=json`);
-  }, []);
-
   return (
     <div className="MapContent">
       <MapSLO />
--- packages/frontend/src/map-contents/planted-dashboard/recently-planted-card/RecentlyPlantedCard.tsx
+++ packages/frontend/src/map-contents/planted-dashboard/recently-planted-card/RecentlyPlantedCard.tsx
@@ -2,17 +2,25 @@ import { Typography } from '@mui/material';
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent';
 import { IRecentlyPlantedRecord } from '../PlantedDash';
+import { Link } from 'react-router-dom';
 import './RecentlyPlantedCard.scss';
+import { TREE_NUMBER_QUERY_PARAM } from '../../Map/MapSLO';
 
 interface IProps extends IRecentlyPlantedRecord {}
 
 export const RecentlyPlantedCard = ({ img, treeSpecies, treeNumber, datePlanted, name }: IProps) => {
+  const link = `/?${TREE_NUMBER_QUERY_PARAM}=${treeNumber}#Map`;
+
   return (
     <Card>
       <CardContent>
-        <img className='recently-planted-img' src={img} alt={`Tree ${treeNumber}`} />
+        <Link className='recently-planted-img-container' to={link}>
+          <img className='recently-planted-img' src={img} alt={`Tree ${treeNumber}`} />
+        </Link>
         <Typography className='recently-planted-left recently-planted-tree-number' variant='h5' >
-          {`#${treeNumber}`}
+          <Link to={link}>
+            {`#${treeNumber}`}
+          </Link>
         </Typography>
         <Typography className='recently-planted-left recently-planted-tree-species' variant='h5' >
           {treeSpecies}
