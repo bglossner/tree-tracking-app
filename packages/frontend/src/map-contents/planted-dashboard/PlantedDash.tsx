54edeca296df69bccd2520bd50100c86764075d6
bglossner <benglossner@gmail.com>
2022-06-10 23:11:33 -0700

    feat(frontend): Add documentation and some styling fixes for ticker on mobile

--- packages/frontend/src/map-contents/planted-dashboard/PlantedDash.tsx
+++ packages/frontend/src/map-contents/planted-dashboard/PlantedDash.tsx
@@ -8,27 +8,47 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { usePrevious } from '../../util/hooks/usePrevious';
 import { useWindowSize } from '../../util/hooks/useWindowInfo';
 
+// Number of trees to be displaying at once on ticker
 const CARDS_DISPLAYED = 5;
+
+// this is the number of trees to retrieve at the start
 const TREES_TO_RETRIEVE = 40;
+
+// this is the number of trees to retrieve iteratively as the user
+// clicks "next"
 const TREES_TO_RETRIEVE_INCREMENTAL = 20;
 
 interface IProps {
+  /**
+   * Number of trees currently planted
+   */
   currentPlanted: number;
 }
 
 export interface IRecentlyPlantedRecord {
+  /**
+   * Source of attachment for image. Base64 encoded string or URL
+   */
   img: string;
   treeSpecies: string;
   treeNumber: number;
   datePlanted: Date;
+  /**
+   * Name of planter
+   */
   name: string;
 }
 
+/**
+ * Converts given relatively uncleaned retrieved data from ArcGIS into proper props
+ * value to be passed into RecentlyPlantedCard
+ */
 const dataProcessor = ({
   img, verified_tree_species, date_planted, name_publicly, objectid, recorded_tree_species
 }: IRetrievedData): IRecentlyPlantedRecord => {
   return {
     img: img.src,
+    // tree species, if known
     treeSpecies: verified_tree_species || recorded_tree_species || 'Currently Unknown',
     name: name_publicly,
     datePlanted: new Date(date_planted),
@@ -36,17 +56,28 @@ const dataProcessor = ({
   }
 };
 
+/**
+ * Ticker for showing the recently planted trees
+ */
 export const PlantedDashboard = ({ currentPlanted }: IProps) => {
   const [retrievedTrees, setRetrievedTrees] = useState<IRecentlyPlantedRecord[]>([]);
+  // Used to control whether a request is in progress and to lock the useEffect from retrieving more trees
   const [retrievalLock, setRetrievalLock] = useState(false); 
+  // The current tree number to be viewing on ticker
+  // 0 indicates the most recently planted
   const [currentPage, setCurrentPage] = useState(0);
+  // Whether all the trees have been loaded that exist in ArcGIS
   const [loadedAllData, setLoadedAllData] = useState(false);
+  // Whether to disable the "Next" button
   const [rightBtnDisabled, setRightBtnDisabled] = useState(true);
   const { isDesktop } = useWindowSize();
 
   const prevPage = usePrevious(currentPage);
 
+  // Move dashboard in given direction
+  // -1 indicates going forward 5 trees (previous hit), 1 indicates going back 5 trees (next hit)
   const onPaginationDirectionClicked = (direction: -1 | 1) => {
+    // Cannot be less than 0
     setCurrentPage(Math.max(currentPage + (direction * CARDS_DISPLAYED), 0));
   };
 
@@ -54,6 +85,7 @@ export const PlantedDashboard = ({ currentPlanted }: IProps) => {
     return currentPage + CARDS_DISPLAYED >= retrievedTrees.length;
   };
 
+  // Retrieves the CARDS_DISPLAYED number of trees based on currentPage
   const getActiveTrees = (): IRecentlyPlantedRecord[] => {
     if (retrievedTrees.length === 0) {
       return [];
@@ -61,6 +93,7 @@ export const PlantedDashboard = ({ currentPlanted }: IProps) => {
 
     return retrievedTrees.slice(
       currentPage,
+      // Cannot be more than the retrieved trees
       Math.min(
         currentPage + CARDS_DISPLAYED,
         retrievedTrees.length
@@ -68,17 +101,23 @@ export const PlantedDashboard = ({ currentPlanted }: IProps) => {
     );
   };
 
+  // Used to load data on a "Next" clicked to view trees that have not been viewed before
   useEffect(() => {
+    // No need to load anymore data
     if (loadedAllData) {
       setRightBtnDisabled(false);
       return;
     }
 
     let numTreesToRetrieve: number;
-    const start = retrievedTrees.length;
+    // Where the pagination should start
+    const numberOfTreesRetrieved = retrievedTrees.length;
 
+    // If we haven't retrieved any trees, retrieve TREES_TO_RETRIEVE amount
     if (currentPage === 0 && retrievedTrees.length === 0) {
       numTreesToRetrieve = TREES_TO_RETRIEVE;
+    // if there was a previous number and that number is greater than current one, the user hit 'Back' btn
+    // and we do nothing
     } else if (prevPage && prevPage > currentPage) {
       setRightBtnDisabled(false);
       return;
@@ -89,15 +128,17 @@ export const PlantedDashboard = ({ currentPlanted }: IProps) => {
       return;
     }
 
+    // If we are retrieving trees already, stop
     if (retrievalLock) {
       return;
     } else {
       setRetrievalLock(true);
     }
 
+    // If data is being loaded, don't let user hit "Next" to prevent spam
     setRightBtnDisabled(true);
 
-    retrieveRecentlyPlantedData(start, numTreesToRetrieve)
+    retrieveRecentlyPlantedData(numberOfTreesRetrieved, numTreesToRetrieve)
       .then((data) => {
         if (data.length < numTreesToRetrieve) {
           setLoadedAllData(true);
@@ -108,6 +149,8 @@ export const PlantedDashboard = ({ currentPlanted }: IProps) => {
       })
       .catch((e) => {
         console.log(e);
+        setRightBtnDisabled(false);
+        setRetrievalLock(false);
       });
   }, [currentPage, retrievedTrees, loadedAllData, prevPage, retrievalLock]);
 
@@ -125,6 +168,7 @@ export const PlantedDashboard = ({ currentPlanted }: IProps) => {
       <div className='card-holder'>
         {getActiveTrees().map((record, idx) => <RecentlyPlantedCard key={`rec-planted-${idx}`} {...record} />)}
       </div>
+      {/* Left and Right buttons */}
       <div className='pagination-container'>
         {currentPage !== 0 && (
           <IconButton onClick={() => onPaginationDirectionClicked(-1)}>
--- packages/frontend/src/map-contents/planted-dashboard/recently-planted-card/DataProcessor.tsx
+++ packages/frontend/src/map-contents/planted-dashboard/recently-planted-card/DataProcessor.tsx
@@ -5,9 +5,15 @@ import imageNotFound from '../../../assets/images/no-image-found.jpg';
 import './RecentlyPlantedCard.scss';
 
 interface ImageInfo {
+  /**
+   * Url referencing the image
+   */
   src: string;
 }
 
+/**
+ * Mapping between an object ID and the attachment
+ */
 type ImageMap = Record<string, ImageInfo>;
 
 interface ObjectAttachment {
@@ -24,8 +30,12 @@ export interface IRetrievedData {
   img: ImageInfo;
 };
 
+// Made global so one instance is made for convenience
 let layer: FeatureLayer | null = null;
 
+/**
+ * Retrieves the feature layer info from ArcGIS for the JS API to get attachments
+ */
 function getLayer(): FeatureLayer {
   if (!layer) {
     layer = new FeatureLayer({
@@ -41,11 +51,17 @@ function getLayer(): FeatureLayer {
   }
 }
 
+/**
+ * Retrieves the tree data from ArcGIS of the next @pageLength trees starting from @page
+ * @param page Reference to ArcGIS pagination start
+ * @param pageLength Number of trees to retrieve
+ */
 async function getData(page: number, pageLength: number): Promise<IFeature[]>  {
   const lastTreesPlanted = await queryFeatures({
     url: PUBLIC_VIEW_URL,
     resultOffset: page,
     resultRecordCount: pageLength,
+    // Only want these fields
     outFields: ['objectid', 'name_publicly', 'date_planted', 'verified_tree_species', 'tree_species', 'verified'],
     returnGeometry: false,
     orderByFields: "objectid DESC",
@@ -54,7 +70,11 @@ async function getData(page: number, pageLength: number): Promise<IFeature[]>  {
   return lastTreesPlanted.features;
 }
 
+/**
+ * Retrieves the relevant attachment (tree image) for each object ID
+ */
 async function getImages(objectIds: number[]): Promise<ImageMap> {
+  // retrieves images only. MUST sort the objectIds
   const attachments: Record<string, ObjectAttachment[]> = await getLayer().queryAttachments({
     attachmentTypes: ["image/jpeg", "image/png", "image/jpg"],
     objectIds: objectIds.sort(),
@@ -62,10 +82,13 @@ async function getImages(objectIds: number[]): Promise<ImageMap> {
 
   const images: ImageMap = {};
 
+  // Convert to string so key is string for JSON (not needed but would be done implicitly)
   objectIds.map(o => o.toString()).forEach((objectId) => {
     const objAttachments = attachments[objectId];
 
     images[objectId] = {
+      // the source is the first attachment related to that object id or an image that tells the user the
+      // tree has no image
       src: objAttachments?.[0].url ?? imageNotFound,
     };
   });
@@ -73,16 +96,24 @@ async function getImages(objectIds: number[]): Promise<ImageMap> {
   return images;
 }
 
+/**
+ * Retrieve the data and images for the @pageLength number of trees starting pagination from @page
+ */
 export async function retrieveRecentlyPlantedData(page: number, pageLength: number): Promise<IRetrievedData[]> {
   const dataWithoutImages = await getData(page, pageLength);
+
+  if (dataWithoutImages.length === 0) {
+    return [];
+  }
+
   const objectIds = dataWithoutImages.map((entry) => (entry.attributes.objectid));
   const images = await getImages(objectIds);
 
   return dataWithoutImages.map((data) => {
     return {
-      verified: data.attributes.Verified as string,
+      verified: data.attributes.verified as string,
       objectid: +data.attributes.objectid,
-      recorded_tree_species: data.attributes.field_17 as string,
+      recorded_tree_species: data.attributes.tree_species as string,
       date_planted: data.attributes.date_planted as string,
       verified_tree_species: data.attributes.verified_tree_species as string,
       name_publicly: data.attributes.name_publicly as string,
